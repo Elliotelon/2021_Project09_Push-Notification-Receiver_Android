3edb0fc11a7ac7b058b0a6803b60538cdf5cc08f
kimminkyu <kmk50009@naver.com>
2021-05-05 11:49:34 +0900

    Push Notification Receiver Notification 구현

--- app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.aop.part3.chapter01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -21,6 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -30,5 +38,14 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false){
+        resultTextView.text =(intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if(isNewIntent){
+                    "(으)로 갱신했습니다."
+                }else{
+                    "(으)로 실행했습니다."
+                }
     }
 }
--- app/src/main/java/fastcampus/aop/part3/chapter01/MyFireBaseMessagingService.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MyFireBaseMessagingService.kt
@@ -1,5 +1,16 @@
 package fastcampus.aop.part3.chapter01
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -9,7 +20,93 @@ class MyFireBaseMessagingService: FirebaseMessagingService() {
         super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        createNotificationChannel()
+
+        val type = remoteMessage.data["type"]
+            ?.let{NotificationType.valueOf(it)}
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+
+        NotificationManagerCompat.from(this)
+            .notify(type.id, createNotification(type, title, message))
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(
+        type: NotificationType,
+        title: String?,
+        message: String?
+    ): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply{
+            putExtra("notificationType","${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, type.id, intent, FLAG_UPDATE_CURRENT)
+
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_none_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        when(type){
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "😣 😖 😫 😩 🥺 😢 😭 😤 😠 😡 🤬 🤯 " +
+                                    "😳 🥵 🥶 😱 😨 😰 😥 😓 🤗" +
+                                    " 🤔 🤭 🤫 🤥 😶 😐 😑"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply {
+                            setTextViewText(R.id.title, title)
+                            setTextViewText(R.id.message, message)
+                        }
+                    )
+            }
+
+        }
+
+        return notificationBuilder.build()
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
     }
 }
